ce7a9f7db70940cd101ebc44a2cc141a75e29d12
Daniel Kofi Kyeremateng <kofwayne305@gmail.com>
2020-04-14 10:12:21 +0000

    update-one

--- src/estimator.js
+++ src/estimator.js
@@ -5,7 +5,7 @@ const getPercentage = (percent) => percent / 100;
 const covid19ImpactEstimator = (data) => {
   const input = data;
   const impacts = {};
-  const severeImpacts = severeImpactData(input);
+  const severeImpacts = {};
 
   impacts.currentlyInfected = data.reportedCases * 10;
   impacts.infectionsByRequestedTime = impacts.currentlyInfected * 512;
@@ -23,6 +23,25 @@ const covid19ImpactEstimator = (data) => {
   );
   impacts.dollarsInFlight = Math.trunc(
     (impacts.infectionsByRequestedTime * 0.65 * 1.5) / 30
+  );
+
+  severeImpacts.currentlyInfected = data.reportedCases * 50;
+  severeImpacts.infectionsByRequestedTime =
+    severeImpacts.currentlyInfected * 512;
+  severeImpacts.severeCasesByRequestedTime = Math.trunc(
+    getPercentage(15) * severeImpacts.infectionsByRequestedTime
+  );
+  severeImpacts.hospitalBedsByRequestedTime = Math.trunc(
+    getPercentage(35) * (data.totalHospitalBeds - severeImpacts.severeCasesByRequestedTime)
+  );
+  severeImpacts.casesForICUByRequestedTime = Math.trunc(
+    getPercentage(5) * severeImpacts.infectionsByRequestedTime
+  );
+  severeImpacts.casesForVentilatorsByRequestedTime = Math.trunc(
+    getPercentage(2) * severeImpacts.infectionsByRequestedTime
+  );
+  severeImpacts.dollarsInFlight = Math.trunc(
+    (severeImpacts.infectionsByRequestedTime * 0.65 * 1.5) / 30
   );
 
   return {
