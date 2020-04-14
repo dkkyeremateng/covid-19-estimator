e04a48f0eae9c5f60f59a66732406222bf5bdf2b
Daniel Kofi Kyeremateng <kofwayne305@gmail.com>
2020-04-14 09:54:55 +0000

    challenge-one-update-four

--- src/estimator.js
+++ src/estimator.js
@@ -3,11 +3,13 @@ const severeImpactData = require('./severeImpact');
 
 const covid19ImpactEstimator = (data) => {
   const input = data;
+  const impacts = impactData(input);
+  const severeImpacts = severeImpactData(input);
 
   return {
     data: input, // the input data you got
-    impact: impactData(input), // your best case estimation
-    severeImpact: severeImpactData(input) // your severe case estimation
+    impact: impacts, // your best case estimation
+    severeImpact: severeImpacts // your severe case estimation
   };
 };
 
